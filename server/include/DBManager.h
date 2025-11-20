aa3da9e3940cde272550b76107f6ae82e4d258c8
seonseohyun <seonseo1216@gmail.com>
2025-11-21 00:55:29 +0900

    build: add MariaDB

--- server/include/DBManager.h
+++ server/include/DBManager.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <string>
+#include <memory>
+#include <mariadb/mysql.h>
 
 class DBManager {
 public:
     DBManager();
+    ~DBManager();
+
     bool checkLogin(const std::string& id, const std::string& pw);
-    bool createUser(const std::string& id, const std::string& pw, const std::string& email);
+//     bool createUser(const std::string& id, const std::string& pw, const std::string& email);
 
 private:
-    void connect();
+    MYSQL* conn; //커넥터 핸들
+    bool connect(); //연결
 };
