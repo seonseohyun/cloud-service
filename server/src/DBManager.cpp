86c8f4aa3a86e0ee0c5cfd2fb6761f83f656acb8
seonseohyun <seonseo1216@gmail.com>
2025-11-21 19:16:13 +0900

    feat(auth): add /signup endpoint

--- server/src/DBManager.cpp
+++ server/src/DBManager.cpp
@@ -147,3 +147,78 @@ bool DBManager::checkLogin(const string& id, const string& pw) {
     return count > 0;
 }  
 
+bool DBManager::createUser(const string& id, const string& pw, const string& name, const string& email) {
+    if (!connect())
+       return false;
+
+   // 1) 스테이트먼트 초기화
+   StmtGuard stmt(conn);
+   if (!stmt.get()){
+       cerr << "[DB] stmt init failed: " << mysql_error(conn) << endl;
+       return false;
+   }
+
+   //2) SQL 준비
+   const char* sql =
+       "INSERT INTO users (id, pw, name, email) VALUES (?, ?, ?, ?);";
+       if (mysql_stmt_prepare(stmt, sql, strlen(sql)) != 0) {
+           cerr << "[DB] prepare failed: "<< mysql_stmt_error(stmt) << "\n";
+       return false;
+   }
+
+   // 3) 파라미터 바인딩
+   MYSQL_BIND param[4];
+   memset(param, 0, sizeof(param));
+
+   unsigned long id_len = id.size();
+   unsigned long pw_len = pw.size();
+   unsigned long name_len = name.size();
+   unsigned long email_len = email.size();
+
+   // Parameter 0 : id
+   param[0].buffer_type   = MYSQL_TYPE_STRING;
+   param[0].buffer        = (void*)id.c_str();
+   param[0].buffer_length = id_len;
+   param[0].length        = &id_len;
+
+   // Parameter 1 : pw
+   param[1].buffer_type   = MYSQL_TYPE_STRING;
+   param[1].buffer        = (void*)pw.c_str();
+   param[1].buffer_length = pw_len;
+   param[1].length        = &pw_len;
+
+   // Parameter 2 : name
+   param[2].buffer_type   = MYSQL_TYPE_STRING;
+   param[2].buffer        = (void*)name.c_str();
+   param[2].buffer_length = name_len;
+   param[2].length        = &name_len;
+
+   // Parameter 3 : email
+   param[3].buffer_type   = MYSQL_TYPE_STRING;
+   param[3].buffer        = (void*)email.c_str();
+   param[3].buffer_length = email_len;
+   param[3].length        = &email_len;
+
+    if (mysql_stmt_bind_param(stmt, param) != 0) {
+        cerr << "[DB] bind_param 오류: "<< mysql_stmt_error(stmt) << "\n";  
+        return false;
+    }
+
+    // 4) 실행
+    //mysql_stmt_execute는 0이 정상
+    if (mysql_stmt_execute(stmt) != 0) {
+        unsigned int err = mysql_stmt_errno(stmt);
+
+        // 1062: Duplicate entry - unique 제약조건 위반
+        if (err == 1062) {
+            cerr << "[DB] 중복된 ID입니다: " << mysql_stmt_error(stmt) << "\n";
+            return false;
+        }
+        else {
+            cerr << "[DB] 실행 오류: " << mysql_stmt_error(stmt) << "\n";
+            return false;
+        }
+    }
+    return true;
+}
+
